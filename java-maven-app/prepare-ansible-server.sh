488b03cd53d58254893b0f3e17ed1dbcf68151c8
Unicorn <lala.la.flaca11@gmail.com>
2025-10-15 15:05:32 -0500

    Adding new droplet

--- java-maven-app/prepare-ansible-server.sh
+++ java-maven-app/prepare-ansible-server.sh
@@ -6,19 +6,6 @@ echo 'running script'
 apt-get update -y
 apt-get install -y ansible python3-boto3 python3-botocore unzip curl jq
 
-
-# Install AWS CLI v2 if missing
-if ! command -v aws >/dev/null 2>&1; then
-  curl -sS "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
-  unzip -q awscliv2.zip
-  ./aws/install || sudo ./aws/install
-  rm -rf aws awscliv2.zip
-fi
-
-# Ansible collection for dynamic inventory
-ansible-galaxy collection install amazon.aws
-
-
 # SSH key perms (assumes Jenkins copied it to /root)
 chmod 400 /root/ssh-key.pem || true
 
@@ -28,3 +15,31 @@ ansible --version
 echo "Checking AWS CLI"
 aws --version
 aws sts get-caller-identity
+
+#Creating credentials file
+mkdir -p ~/.aws
+chmod 700 ~/.aws
+
+# ensure AWS keys are provided
+if [[ -z "${AWS_ACCESS_KEY_ID:-}" || -z "${AWS_SECRET_ACCESS_KEY:-}" ]]; then
+  echo "ERROR: AWS credentials are not set"
+  exit 1
+fi
+
+# create credentials file
+cat > ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOF
+chmod 600 ~/.aws/credentials
+
+# create aws comfig file
+cat > ~/.aws/config <<EOF
+[default]
+region = us-east-2
+output = json
+EOF
+chmod 600 ~/.aws/config
+
+echo "✅ AWS credentials configured"
