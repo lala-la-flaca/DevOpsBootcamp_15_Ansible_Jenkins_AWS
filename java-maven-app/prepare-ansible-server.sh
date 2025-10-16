83d79049bc51ee71d371cf3168ffe75a6a2644a6
Unicorn <lala.la.flaca11@gmail.com>
2025-10-16 13:00:44 -0500

    full automation ansible server and ec2 test

--- java-maven-app/prepare-ansible-server.sh
+++ java-maven-app/prepare-ansible-server.sh
@@ -6,8 +6,9 @@ echo 'running script'
 apt-get update -y
 apt-get install -y ansible python3-boto3 python3-botocore unzip curl jq
 
-# SSH key perms (assumes Jenkins copied it to /root)
-chmod 400 /root/ssh-key.pem || true
-
+echo "Ansible version: "
 ansible --version
 
+# Create AWS credentials directory
+mkdir -p /root/.aws
+chmod 700 /root/.aws
